89583a221cc7a907d9d8936c3ffb86211fdcd433
toxic14 <75319001+toxic14@users.noreply.github.com>
2021-06-20 08:18:05 -0700

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,23 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 700,100)
+    fill("lightblue")
+    text("Hint: Performs some task !!", 700,120)
+    fill("white")
+    textSize(15)
+    text("A T A D A S B A", 100,300)
+    fill("lightblue")
+    text("Hint: Stores some data !!", 100,320)
+
+}
